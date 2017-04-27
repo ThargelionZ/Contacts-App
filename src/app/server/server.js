cab31a7e1aebbba3a3ac7b5a730829d77c79dd3d
ThargelionZ <measles10@gmail.com>
2017-04-27 17:46:47 -0600

    Added mongo into the app

--- src/app/server/server.js
+++ src/app/server/server.js
@@ -2,15 +2,29 @@
  * Created by mylesparker on 4/3/17.
  */
 
-var fs = require("fs");
+const fs = require("fs");
 
-var express = require("express");
+const express = require("express");
+const app = express();
 
-var app = express();
+const cors = require("cors");
 
-var cors = require("cors");
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
+const url = 'mongodb://localhost:27017/contactsDatabase';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to mongo server: " + url);
+  //removeDocument(db, function() {});
+  //insertDocuments(db, function() {});
+  //db.dropDatabase();
+  //findDocuments(db, function() {});
+  db.close();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -128,3 +142,71 @@ function sortDescending(JSONcontacts) {
   contacts = JSON.stringify(contacts);
   return contacts;
 }
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('contacts');
+  let contacts = getContacts();
+  contacts = JSON.parse(contacts);
+  // Insert some documents
+  collection.insertMany([
+    {a: 1}, {b: 2}, {c: 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+};
+//
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('contacts');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs);
+    callback(docs);
+  });
+};
+//
+// var findDocuments = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Find some documents
+//   collection.find({'a': 3}).toArray(function(err, docs) {
+//     assert.equal(err, null);
+//     console.log("Found the following records");
+//     console.log(docs);
+//     callback(docs);
+//   });
+// };
+//
+// var updateDocument = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Update document where a is 2, set b equal to 1
+//   collection.updateOne({ a : 2 }
+//     , { $set: { b : 1 } }, function(err, result) {
+//       assert.equal(err, null);
+//       assert.equal(1, result.result.n);
+//       console.log("Updated the document with the field a equal to 2");
+//       callback(result);
+//     });
+// };
+//
+var removeDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('contacts');
+  // Delete document where a is 3
+  collection.deleteOne({}, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });
+};
+
+
